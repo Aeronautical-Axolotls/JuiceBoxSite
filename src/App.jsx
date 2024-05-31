59fe9a4aa3ef70b0de2069d6f2ebaed61da13f30
Dillon-Goicoechea <77130346+Dillon-Goicoechea@users.noreply.github.com>
2024-05-31 10:06:27 -0700

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
-import { useState } from 'react'
+import Logo from './assets/juicebox_logo_256.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+          <img src={Logo} className="logo" alt="logo" />
       </div>
+      <h1>Vite + React</h1>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
